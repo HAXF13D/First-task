ae98d3e3d14981a13fba33807a0400ab3cbff718
HAXF13D <vovik0312@gmail.com>
2023-05-29 01:00:23 +0300

    Revert "First task"
    This reverts commit 060427a2f510beddb82657f533279b6379655ca6.

--- API_test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-r = requests.post('http://127.0.0.1:8000/questions/', json={'questions_amount': '4'})
-print(r.status_code)
--- credits.py
+++
@@ -1,3 +0,0 @@
-USER_NAME = "postgres"
-PASSWORD = "1234"
-DB_NAME = "test_task_1"
